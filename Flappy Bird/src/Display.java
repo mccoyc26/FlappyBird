364a19b674b040172d25914c473180ee400baaf2
1915824 <1915824@01-18109.pusd.dom>
2025-05-21 11:56:52 -0700

    push

--- Flappy Bird/src/Display.java
+++ Flappy Bird/src/Display.java
@@ -1,18 +1,34 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.net.URL;
 
 import javax.swing.JPanel;
 
-class Display extends JPanel{
+public class Display extends JPanel{
 
 	public static final int WIDTH = 1280;
 	public static final int HEIGHT = 720;
+	
+	Image bird;
+	Image tPipe;
+	Image bPipe;
+	Image bgr;
 	
 	public Display() {
 		//create display settings
 		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		this.setBackground(Color.DARK_GRAY);
 		this.setLayout(null);
+		
+	}
+	
+	public void Paint() {
+		
+		
+		
+		
 	}
 	
 }
